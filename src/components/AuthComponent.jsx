6585ee3887c3de8c3412148a705a6c65691aa578
Sandeep Shrestha <designer@Geniuss-Mac-mini.local>
2025-02-28 10:59:22 +0545

    [Remove] Context reaminings

--- src/components/AuthComponent.jsx
+++ src/components/AuthComponent.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
-import { Outlet, useOutletContext } from "react-router-dom";
 import LoginComponent from './commonComponents/LoginComponent'
 import RegisterComponent from './commonComponents/RegisterComponent'
 
 export default function AuthComponent() {
-
-    const status = useOutletContext();
-
-    console.log(status + "auth");
-
     return (
             <div className={`authentication`}>
                 <div className="authentication__content">
                     <div className="authentication__close">
                         <i className="bi bi-x"></i>
                     </div>
-                    <Outlet context={status} />
                     <LoginComponent />
                     <RegisterComponent />
                 </div>
